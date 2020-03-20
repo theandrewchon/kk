ee8bbc7af1c391c1262cb11fd9af5708c20bdbfb
Andrew Chon <chonclan@gmail.com>
2020-03-19 22:13:57 -0500

    Updates

---
+++ precache-manifest.b7b25229a0ab5c6e911e569cdb8e4493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660971e931db83d3380948e51b5d442f",
+    "url": "/kk/index.html"
+  },
+  {
+    "revision": "d79fe5fd929471920f19",
+    "url": "/kk/static/css/main.d191d81b.chunk.css"
+  },
+  {
+    "revision": "8ac836597623c7b1ce99",
+    "url": "/kk/static/js/2.24bf2af1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kk/static/js/2.24bf2af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79fe5fd929471920f19",
+    "url": "/kk/static/js/main.984fce2b.chunk.js"
+  },
+  {
+    "revision": "f327668826b0d645ef6e",
+    "url": "/kk/static/js/runtime-main.f5244227.js"
+  }
+]);
